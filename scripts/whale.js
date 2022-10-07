d018c8da9e1d73e715875b58c1e21e204fae3565
Nick Laiacona <nick@performantsoftware.com>
2022-10-07 11:43:03 -0400

    parse text teidoc file

--- scripts/whale.js
+++ scripts/whale.js
@@ -17,14 +17,12 @@ function dirExists( dir ) {
     }  
 }
 
-function convertToHTML( sourcePath ) {
+function convertToHTML( contentXML ) {
     const htmlDOM = new JSDOM()
     const ceTEI = new CETEI(htmlDOM.window)
 
-    console.log(`Converting ${sourcePath}`)
     try {
-        const xml = fs.readFileSync(sourcePath, "utf8")
-        const xmlDOM = new JSDOM(xml, { contentType: "text/xml" })    
+        const xmlDOM = new JSDOM(contentXML, { contentType: "text/xml" })    
         const data = ceTEI.domToHTML5(xmlDOM.window.document)
         return data.innerHTML
     } catch( err ) {
@@ -48,54 +46,120 @@ function clearDir(targetPath) {
     }
 }
 
-function renderText(id, name, localID, targetPath) {
-    const sourceFile = `resources/${id}`
-    const content = convertToHTML(sourceFile)
-    const html = pageTemplate(name,content)
+function renderText(localID, contentXML, targetPath) {
+    const content = convertToHTML(contentXML)
+    const html = pageTemplate(localID,content)
     const targetFile = `${targetPath}/${localID}.html`
     fs.writeFileSync(targetFile, html, "utf8")        
 }
 
-function renderHeader(parentName, targetPath) {
-    const markdown = headerTemplate(parentName)
+function renderHeader(header, targetPath) {
+    const markdown = headerTemplate(header.title)
     const targetFile = `${targetPath}/_index.md`
     fs.writeFileSync(targetFile, markdown, "utf8")        
 }
 
-function renderTEIDoc(parentID, parentName, parentLocalID, resources, targetPath) {
-
-    const teiDocPath = `${targetPath}/${parentLocalID}`
+function renderTEIDoc(teiDoc, targetPath) {
+    const teiDocPath = `${targetPath}/${teiDoc.id}`
     dirExists(teiDocPath)
 
     // first render the top level page
-    renderHeader(parentName,teiDocPath)
+    renderHeader(teiDoc.header, targetPath)
 
     // then render the children
-    for( const resource of resources ) {
-        const { resource_guid: id, name, local_id: localID, resource_type: resourceType, parent_guid: parentResource } = resource
-        if( parentResource === parentID && resourceType === 'text' ) {
-            renderText(id, name, localID, teiDocPath)
+    for( const resource of teiDoc.resources ) {
+        const { local_id: localID, resource_type: resourceType, content } = resource
+        if( resourceType === 'text' ) {
+            renderText(localID, content, teiDocPath)
         }
     }
+}
+
+function parseTEIDoc(teidocID, teidocXML) {
+    // pull out resources and create resourceMap
+    
+    const xmlDom = new JSDOM(teidocXML, { contentType: "text/xml" }).window.document
+
+    const teiEl = xmlDom.getElementsByTagName('tei')[0] || xmlDom.getElementsByTagName('TEI')[0]
+    if( !teiEl ) {
+        throw new Error('Document must contain a <TEI> element.')
+    }
+    const teiHeaderEl = teiEl.getElementsByTagName('teiheader')[0] || teiEl.getElementsByTagName('teiHeader')[0]
+
+    let textEls = teiEl.getElementsByTagName('text') 
+    if( textEls.length === 0 ) textEls = teiEl.getElementsByTagName('TEXT')
+
+    let standOffEls = teiEl.getElementsByTagName('standOff') 
+    if( standOffEls.length === 0 ) standOffEls = teiEl.getElementsByTagName('STANDOFF')
+
+    let sourceDocEls = teiEl.getElementsByTagName('sourceDoc') 
+    if( sourceDocEls.length === 0 ) sourceDocEls = teiEl.getElementsByTagName('SOURCEDOC')
+
+    let facsEls = teiEl.getElementsByTagName('facsimile') 
+    if( facsEls.length === 0 ) facsEls = teiEl.getElementsByTagName('FACSIMILE')
+
+    if( textEls.length === 0 && facsEls.length === 0 && standOffEls.length === 0 && sourceDocEls.length === 0 ) {
+        throw new Error('<TEI> element must contain one more more <text>, <standOff>, <sourceDoc>, or <facsimile> elements.')
+    } 
+
+    const resources = []
+    for( let i=0; i < textEls.length; i++ ) {
+        const contentEl = textEls[i]
+        const resource_guid = `${teidocID}-text${i}`
+        const local_id = contentEl.getAttribute('xml:id')
+        const resource_type = 'text'
+        const content = contentEl.outerHTML
+        const resource = { resource_guid, local_id, resource_type, content }
+        resources.push(resource)
+    }
+    for( let i=0; i < standOffEls.length; i++ ) {
+        const contentEl = standOffEls[i]
+        const resource_guid = `${teidocID}-sourceDoc${i}`
+        const local_id = contentEl.getAttribute('xml:id')
+        const resource_type = 'sourceDoc'
+        const content = contentEl.outerHTML
+        const resource = { resource_guid, local_id, resource_type, content }
+        resources.push(resource)
+    }
+    for( let i=0; i < sourceDocEls.length; i++ ) {
+        const contentEl = sourceDocEls[i]
+        const resource_guid = `${teidocID}-standOff${i}`
+        const local_id = contentEl.getAttribute('xml:id')
+        const resource_type = 'standOff'
+        const content = contentEl.outerHTML
+        const resource = { resource_guid, local_id, resource_type, content }
+        resources.push(resource)
+    }
+    for( let i=0; i < facsEls.length; i++ ) {
+        const contentEl = facsEls[i]
+        const resource_guid = `${teidocID}-facs${i}`
+        const local_id = contentEl.getAttribute('xml:id')
+        const resource_type = 'facs'
+        const content = contentEl.outerHTML
+        const resource = { resource_guid, local_id, resource_type, content }
+        resources.push(resource)
+    }
+
+    const resource_guid = `${teidocID}-header`
+    const local_id = teiHeaderEl.getAttribute('xml:id')
+    const titleEl = teiHeaderEl.getElementsByTagName('title')[0]
+    const title = titleEl ? titleEl.innerText : 'untitled'
+    const resource_type = 'teiheader'
+    const content = teiHeaderEl.outerHTML
+    const header = { resource_guid, title, local_id, resource_type, content }
+
+    return { id: teidocID, header, resources }
 }
 
 async function processDocs(targetPath) {
     clearDir(targetPath)
 
     // generate list of sourceFiles
-    const resourceMapJSON = fs.readFileSync('resources/resource_map.json', 'utf-8')
-    const resourceMap = JSON.parse(resourceMapJSON)
-    const { resources } = resourceMap
+    const exampleXML = fs.readFileSync('resources/WAAC_1_01_1943.xml', 'utf-8')
+    const teiDocs = [ parseTEIDoc('WAAC_1_01_1943',exampleXML) ]
 
-    for( const resource of resources ) {
-        const { resource_guid: id, name, local_id: localID, resource_type: resourceType, parent_guid: parentResource } = resource
-        if( parentResource === null ) {
-            if( resourceType === 'text' ) {
-                renderText(id, name, localID, targetPath) 
-            } else if( resourceType === 'teidoc' ) {
-                renderTEIDoc(id, name, localID, resources, targetPath)
-            }   
-        }
+    for( const teiDoc of teiDocs ) {
+        renderTEIDoc(teiDoc, targetPath)
     }    
 }
 
